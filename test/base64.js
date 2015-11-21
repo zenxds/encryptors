8fd455ce1f8ae1c00fed1f0af4f5aec4556c92d5
Dongshuang.xds <dongshuang.xds@alibaba-inc.com>
2015-11-21 22:47:23 +0800

    add base64

--- test/base64.js
+++ test/base64.js
@@ -1,22 +1,22 @@
 var assert = require('assert');
 var expect = require('expect.js');
 
-var Utf8 = require('../src/utf8');
+var base64 = require('../src/base64');
 
-describe('utf8', function() {
+describe('base64', function() {
 
     it('should encode and decode 1 byte string', function() {
         var str = "abcd";
-        assert.equal(str, Utf8.decode(Utf8.encode(str)));
+        assert.equal(str, base64.decode(base64.encode(str, true), true));
     });
 
     it('should encode and decode 2 bytes string', function() {
         var str = "̢̡";
-        assert.equal(str, Utf8.decode(Utf8.encode(str)));
+        assert.equal(str, base64.decode(base64.encode(str, true), true));
     });
 
     it('should encode and decode 3 bytes string', function() {
         var str = "这是个中文字符串";
-        assert(str, Utf8.decode(Utf8.encode(str)));
+        assert(str, base64.decode(base64.encode(str, true), true));
     });
 });
